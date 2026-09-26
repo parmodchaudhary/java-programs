e700ff377f21a0cebc85790a42d23718c8cfbbdb
parmodchaudhary <parmodchaudhary12467@gmail.com>
2026-09-26 23:45:35 +0530

    Inverted Pyramid pattern add

---
+++ pattern/InvertedPyramid.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class InvertedPyramid {
+   public static void main(String[] args) {
+                   Scanner sc = new Scanner(System.in);
+                   System.out.println("Enter the number of rpws and column");
+                   int n = sc.nextInt();
+             for(int i=1;i<=n;i++){
+            for(int j=i; j<=n;j++){
+System.out.print("*");
+
+            }
+            System.out.println("");
+            
+            
+        }
+    }
+}
